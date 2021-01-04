d3c51cad7d4f4cac9d1c424fc1beaef4bbc01512
Eric Heinzl <e.heinzl@shopware.com>
2021-01-04 14:51:23 +0100

    Fixed broadcast-auth-error by adding the authEndpoint

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -42,6 +42,7 @@ window.Pusher = require('pusher-js');
  * pusher.com
  */
 window.Echo = new Echo({
+    authEndpoint : 'broadcasting/auth',
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
